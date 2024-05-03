1548080b1d09b821c5526c4a4834bd457358b6e8
Kiran Pradeep <kira@Kirans-MacBook-Air.local>
2024-05-03 06:49:57 +0530

    speaker verification metric added

--- metrics/speaker_recognition.py
+++ metrics/speaker_recognition.py
@@ -1,9 +1,25 @@
 # from speechbrain.inference.speaker import SpeakerRecognition
 from speechbrain.inference.speaker import SpeakerRecognition
-
+import os
+import glob
+from tqdm.auto import tqdm
 
 verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
-score, prediction = verification.verify_files(
-    "en-pred/common_voice_it_17440353.wav", 
-    "en-a/common_voice_it_17440353.mp3.wav"
-    )
+
+pred_pred_list = []
+pred_score_list = []
+ac_pred_list = []
+ac_score_list = []
+# Load the WAV files
+audio_path_list = glob.glob('en-pred/*.wav')
+for audio_path in tqdm(audio_path_list):
+    basename = os.path.basename(audio_path)
+    filename = basename.replace('.wav', '')
+    
+    score, prediction = verification.verify_files(
+        f"en-a/{filename}.mp3.wav",
+        f"Italian-short/{filename}.mp3"
+        )
+    ac_score_list.append(score.item())
+
+print('VAM voice preservation score: ', sum(ac_score_list)/len(ac_score_list))
