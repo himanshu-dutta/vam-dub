d773dac62f1e51290b25df8369c54d2a10f2a9f8
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2024-05-03 12:47:20 +0530

    added gpu support for prediction

--- demo/s2st.py
+++ demo/s2st.py
@@ -1,4 +1,8 @@
-import torchaudio
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "5,4"
+
+import torch
 from transformers import (
     AutoProcessor,
     SeamlessM4TModel,
@@ -15,14 +19,16 @@ parser.add_argument("-i", "--input-audio", type=str)
 parser.add_argument("-o", "--output-audio", type=str)
 args = parser.parse_args()
 
-# processor = AutoProcessor.from_pretrained("facebook/hf-seamless-m4t-medium")
-# model = SeamlessM4TModel.from_pretrained("facebook/hf-seamless-m4t-medium")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
 
 processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
-model = SeamlessM4Tv2ForSpeechToSpeech.from_pretrained("facebook/seamless-m4t-v2-large")
+model = SeamlessM4Tv2ForSpeechToSpeech.from_pretrained(
+    "facebook/seamless-m4t-v2-large"
+).to(device)
 
 audio, _ = librosa.load(args.input_audio, sr=16000)
-audio_inputs = processor(audios=audio, return_tensors="pt")
+audio_inputs = processor(audios=audio, return_tensors="pt").to(device)
 audio_array_from_audio = (
     model.generate(**audio_inputs, tgt_lang="eng")[0].cpu().numpy().squeeze()
 )
